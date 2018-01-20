e4cbe0ce5a2a3025347d3622669b6aace53e7b00
wendelfariaslopes <wendellopesfarias@gmail.com>
2018-01-20 13:49:45 -0500

    :coffee: Hungarian Algorithm

---
+++ src/ftp/FTPConferidor.java
@@ -0,0 +1,12 @@
+package ftp;
+
+public class FTPConferidor {
+	
+	//Responsavel pela conferencia da carga do FTP no servidor local da SODF
+	//Verifica a existencia de arquivo
+	//Verifica a data do arquivo 
+	//Confere a identificacao do arquivo
+	// 
+	
+
+}
