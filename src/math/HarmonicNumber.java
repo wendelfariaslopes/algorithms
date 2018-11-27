8c88a6f62306ffd1c736fe47f1cd9784f10c832c
wendelfariaslopes <wendellopesfarias@gmail.com>
2018-11-27 08:40:18 -0500

    Harmonic Number

---
+++ src/math/HarmonicNumber.java
@@ -0,0 +1,19 @@
+package math;
+
+public class HarmonicNumber { 
+    public static void main(String[] args) { 
+
+        // command-line argument
+        int n = Integer.parseInt(args[0]);
+
+        // compute 1/1 + 1/2 + 1/3 + ... + 1/n
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+
+        // print the nth harmonic number
+        System.out.println(sum);
+    }
+
+}
