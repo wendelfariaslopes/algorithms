645077d127c3f7da2ed7831b0dbfe93b593a4458
wendelfariaslopes <wendellopesfarias@gmail.com>
2018-12-19 17:10:52 -0500

    Generic exemple

---
+++ src/algorithms/generic/Portfolio.java
@@ -0,0 +1,23 @@
+package algorithms.generic;
+
+public class Portfolio extends Bean{
+	
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+		
+}
