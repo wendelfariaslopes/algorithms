fb0999ddc2b0eab98be605b96e6b38525ab7dae0
wendelfariaslopes <wendellopesfarias@gmail.com>
2019-02-06 08:45:55 -0500

    Try to do

---
+++ src/algorithms/data/structure/DeepTreeNode.java
@@ -0,0 +1,22 @@
+package algorithms.data.structure;
+
+public class DeepTreeNode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+//	
+//	public int maxDepth(TreeNode root) {
+//	    if(root==null)
+//	        return 0;
+//	 
+//	    int leftDepth = maxDepth(root.left);
+//	    int rightDepth = maxDepth(root.right);
+//	 
+//	    int bigger = Math.max(leftDepth, rightDepth);
+//	 
+//	    return bigger+1;
+//	}
+
+}
---
+++ src/algorithms/deeplearning/Value.java
@@ -0,0 +1,5 @@
+package algorithms.deeplearning;
+
+public class Value {
+
+}
