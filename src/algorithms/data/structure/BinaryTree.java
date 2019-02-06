531118e26b3cbd72ed14b4f5dba4b6e903e2833a
wendelfariaslopes <wendellopesfarias@gmail.com>
2019-02-06 08:45:02 -0500

    Only update

--- src/algorithms/data/structure/BinaryTree.java
+++ src/algorithms/data/structure/BinaryTree.java
@@ -1,75 +1,126 @@
 package algorithms.data.structure;
 
-public class BinaryTree
-{
-    private static Node root;
+public class BinaryTree {
+	
+	public static void main(String[] args) {
+		Node node = createTree();
 
-    public BinaryTree(int data)
-    {
-        root = new Node(data);
-    }
+		System.out.println("Deep = " + maxDepth(node));
+		System.out.println();
+		printTree(node, " ");
 
-    public void add(Node parent, Node child, String orientation)
-    {
-        if (orientation.equals("left"))
-        {
-           parent.setLeft(child);
-        }
-        else
-        {
-            parent.setRight(child);
-        }
-    }
+	}
 
-    public static void main(String args[])
-    {
-        Node n1 = new Node(1);
-        Node n2 = new Node(4);
-        Node n3 = new Node(2);
-        Node n4 = new Node(5);
+	private static Node createTree() {
+		Node node = new Node(1);
 
-        BinaryTree tree = new BinaryTree(3); //  3
-        tree.add(root, n1, "left"); //         1/ \
-        tree.add(root, n2, "right"); //            4
-        tree.add(n2, n3, "left"); //             2/ \
-        tree.add(n2, n4, "right"); //                5
-    }
+		Node nodeL1 = node.addChildLeft(new Node(2));
+		Node nodeL2 = nodeL1.addChildLeft(new Node(4));
+		Node nodeL3 = nodeL1.addChildRight(new Node(6));
+
+		Node nodeR1 = node.addChildRight(new Node(3));
+		Node nodeR2 = nodeR1.addChildRight(new Node(5));
+		Node nodeR3 = nodeR1.addChildLeft(new Node(7));
+
+		return node;
+	}
+
+	private static void printTree(Node node, String appender) {
+		
+		if (node == null) {
+			System.out.println("Nothing....");
+
+		}
+		
+		if(node.getParent()!=null) {
+			System.out.print("("+node.getParent().getKey()+")");
+		}
+		
+		
+		System.out.println(appender + node.getKey());
+		
+		
+		if (node.getLeft() != null) {
+			
+			printTree(node.getLeft(), appender+appender);
+
+		}
+		if (node.getRight() != null) {
+			printTree(node.getRight(), appender+appender);
+
+		}
+		
+	}
+
+	public static int maxDepth(Node node) {
+		if (node == null)
+			return 0;
+
+		int leftDepth = maxDepth(node.getLeft());
+		int rightDepth = maxDepth(node.getRight());
+
+		int bigger = Math.max(leftDepth, rightDepth);
+
+		return bigger + 1;
+	}
+
 }
 
 class Node {
-    
+
 	private int key;
-    private Node left;
-    private Node right;
+	private Node parent;
+	private Node left = null;
+	private Node right = null;
 
-    Node (int key) {
-        this.key = key;
-        right = null;
-        left = null;
-    }
+	public Node(int key) {
+		this.key = key;
+	}
 
-    public void setKey(int key) {
-        this.key = key;
-    }
+	// add child left
+	public Node addChildLeft(Node left) {
+		left.setParent(this);
+		this.left = left;
+		return this.left;
+	}
 
-    public int getKey() {
-        return key;
-    }
+	// add child right
+	public Node addChildRight(Node right) {
+		right.setParent(this);
+		this.right = right;
+		return this.right;
+	}
 
-    public void setLeft(Node left) {
-        this.left = left;
-    }
+	public void setKey(int key) {
+		this.key = key;
+	}
 
-    public Node getLeft() {
-        return left;
-    }
+	public int getKey() {
+		return key;
+	}
 
-    public void setRight(Node right ) {
-        this.right = right;
-    }
+	public Node getParent() {
+		return parent;
+	}
 
-    public Node getRight() {
-        return right;
-    }
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	public Node getRight() {
+		return right;
+	}
 
 }
