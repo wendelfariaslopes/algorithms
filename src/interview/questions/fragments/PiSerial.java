9e8bc956b93f707e8a9db867ea2163809d58e8cc
wendelfariaslopes <wendellopesfarias@gmail.com>
2019-02-19 11:00:15 -0500

    Calculate PI number in parallel

---
+++ src/interview/questions/fragments/PiSerial.java
@@ -0,0 +1,22 @@
+package interview.questions.fragments;
+
+public class PiSerial {
+	
+	private static double f( double x ) {
+		return 4.0 / ( 1 + x*x );
+	}
+	
+	public static double calculate( int N ) {
+
+		if ( N==0 ) N = 1000000;
+		
+		double sum = 0;
+		double deltaX = 1.0/N;
+		
+		//--- iterate ---
+		for ( int i=0; i< N; i++ ) 
+			sum += f( i * deltaX );
+		
+		return sum;
+	}
+}
