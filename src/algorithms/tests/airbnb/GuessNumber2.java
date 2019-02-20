b8bcba3819e66fd3a841096d9568d949ea0b5899
wendelfariaslopes <wendellopesfarias@gmail.com>
2019-02-20 12:34:09 -0500

    Guess number 2

---
+++ src/algorithms/tests/airbnb/GuessNumber2.java
@@ -0,0 +1,51 @@
+package algorithms.tests.airbnb;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+public class GuessNumber2 {
+	
+	int[] range = {1,2,3,4,5,6};
+	private static final int TARGET = 1234;
+	
+	public static void deep(String s) {
+		for (char c : s.toCharArray()) {
+			
+		}
+	}
+	
+	
+	
+	public static boolean validate(int target) {
+		
+		while(validate) {
+			
+		}
+		
+		return true;
+	}
+	
+	public static String guess() {
+		String g = "1234";
+		
+		return g;
+	}
+	
+	 public static class UnitTest {
+	        @Test
+	        public void test() {
+	            String guess = "1234";
+	            assertEquals(guess, guess());
+	            
+
+	            guess = "2234";
+	            assertEquals(guess, guess());
+
+	        }
+	  }
+
+
+}
