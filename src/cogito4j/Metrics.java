464790115b28819b94f46cee4f1988610fba9f6f
wendelfariaslopes <wendellopesfarias@gmail.com>
2020-03-08 21:06:37 -0400

    Chandge package

---
+++ src/cogito4j/Metrics.java
@@ -0,0 +1,109 @@
+package cogito4j;
+
+import java.util.Arrays;
+
+public class Metrics {
+
+	public static void main(String[] args) {
+		double [] actual =   {1, 1, 0, 1, 0, 0, 1, 0, 0, 0}; // Tests made
+		double [] expected = {1, 0, 0, 1, 0, 0, 1, 1, 1, 0}; // name can be expected or predicted (Theory) 
+		
+		// when we refere about Positive cases (P) or Negative cases (N)
+		// We talk about observations about some tests applied over something that we need to measure
+		// If try classify, for example, some set of fruits can be P -> is an apple or N -> is not an apple
+		boolean isTheCase = false; // P or N
+		// When we talk about True Positive (TP), we talk about the observation of something that we considere a priori a "theory
+		// about" or "paradigma about" or "classificado as", for example, we test this fruit, that we already know is an apple,
+		// and we use a test to detect if this give us an apple as a answer
+		// So, True or False are about a priori Theory or Conception or Classification already got.
+		// and Positive or Negative are about a posteriori Tests made about the theory.
+		
+//		int T = 0;
+//		int F = 0;
+//		int P = 0;
+//		int N = 0;
+//		
+//		for(int e=0; e < expected.length; e++) {
+//			if(expected[e]==1) {
+//				++T;
+//			}else {
+//				++F;
+//			}
+//		}
+//		
+//		for(int a=0; a < actual.length; a++) {
+//			if(actual[a]==1) {
+//				++P;
+//			}else {
+//				++N;
+//			}
+//		}
+		
+		int samples = actual.length; // total de samples
+		double TP = 100; //True Positive (TP) : Observation is positive, and is predicted to be positive.
+		//These are cases in which we predicted yes (they have the disease), and they do have the disease.
+		
+		double TN = 50; //True Negative (TN) : Observation is negative, and is predicted to be negative. 
+		//We predicted no, and they don't have the disease.
+		
+		double FP = 10; //False Positive (FP) : Observation is negative, but is predicted positive. 
+		//We predicted yes, but they don't actually have the disease. (Also known as a "Type I error.")
+		
+		double FN = 5; //False Negative (FN) : Observation is positive, but is predicted negative.
+		//We predicted no, but they actually do have the disease. (Also known as a "Type II error.")
+	
+		
+		double[][] confusionMatrix = new double[2][2];
+		
+//		for(int i=0; i < samples; i++) {
+//			if(expected[i]==1 && actual[i]==1) {
+//				++TP;
+//			}
+//			if(expected[i]==0 && actual[i]==1) {
+//				++FP;
+//			}
+//			if(expected[i]==1 && actual[i]==0) {
+//				++TN;
+//			}
+//			
+//			if(expected[i]==0 && actual[i]==0) {
+//				++FN;
+//			}
+//			
+//		}
+		confusionMatrix[1][1]=TP;
+		confusionMatrix[0][1]=FP;
+		confusionMatrix[0][0]=FN;
+		confusionMatrix[1][0]=TN;
+		
+		System.out.println(Arrays.deepToString(confusionMatrix));
+		
+		double total = TP+TN+FP+FN;
+		double actualNo = TN+FP;
+		
+		//Accuracy: Overall, how often is the classifier correct?
+		double accuracy = (TP + TN)/total; //(TP+TN)/total = (100+50)/165 = 0.91
+		//Misclassification Rate: Overall, how often is it wrong? (equivalent to 1 minus Accuracy or also known as "Error Rate")
+		double misclassification = (FP + FN)/(TP+TN+FP+FN);// (FP+FN)/total = (10+5)/165 = 0.09
+		//True Positive Rate: When it's actually yes, how often does it predict yes? (also known as "Sensitivity" or "Recall")
+		double TPR = TP/(FN+TP);// TP/actual yes = 100/105 = 0.95
+		//False Positive Rate: When it's actually no, how often does it predict yes?
+		double FPR = FP/actualNo;// FP/actual no = 10/60 = 0.17
+		//True Negative Rate: When it's actually no, how often does it predict no? (equivalent to 1 minus False Positive Rate, also known as "Specificity")
+		double specificity = TN/actualNo;// TN/actual no = 50/60 = 0.83
+		//Precision: When it predicts yes, how often is it correct?		
+		double precision = TP/(FP+TP); // TP/predicted yes = 100/110 = 0.91
+		//Prevalence: How often does the yes condition actually occur in our sample?
+		double prevalence = (FN+TP)/total; //actual yes/total = 105/165 = 0.64
+		
+		System.out.println("Accuracy          = " + accuracy);
+		System.out.println("Misclassification = " + misclassification);
+		System.out.println("Sensitivity (TPR) = " + TPR);
+		System.out.println("FPR = " + FPR);
+		System.out.println("Specificity (TNR) = " + specificity);
+		System.out.println("Precision = " + precision);
+		System.out.println("Prevalence = " + prevalence);
+		
+	}
+
+}
---
+++ src/cogito4j/RNNTest.java
@@ -0,0 +1,189 @@
+package cogito4j;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import cogito4j.data.DataFactory;
+
+public class RNNTest {
+
+	private static double[][] X;
+	private static double[][] Y;
+
+	private double[][] W1;
+	private double[][] W2;
+	
+	public static List<Integer> listPositiveCases;
+	
+	/*
+	 * -Xmx9000m -Xms6000m
+	 */
+
+	public static void main(String[] args) {
+
+		int samples = 20;
+		int numBytes = 100;
+		
+		
+		Map<Integer,String> modelMap = DataFactory.generatorXY(samples, numBytes);
+
+		System.out.println();
+		System.out.println("---------- Start Training -------------");
+		System.out.println("");
+
+		Map<String, Double[][]> weightMap = learnFunction(X, Y);
+
+		System.out.println();
+		System.out.println("---------- End Training -------------");
+		System.out.println("");
+
+		System.out.println();
+		System.out.println("---------- Start Tests -------------");
+		System.out.println("");
+		
+		double[][] w1 = castDoubleToPrimitiveDouble(weightMap.get("W1"));
+		double[][] w2 = castDoubleToPrimitiveDouble(weightMap.get("W2"));
+		double[][] b1 = castDoubleToPrimitiveDouble(weightMap.get("B1"));
+		double[][] b2 = castDoubleToPrimitiveDouble(weightMap.get("B2"));
+		
+		modelMap.forEach((k,v)->{
+			
+			if(v.equals("Positive")) {
+				
+			}
+		
+		});
+		
+
+		testLearnFunction(X, w1,w2,b1,b2);
+
+
+		System.out.println();
+		System.out.println("---------- End Tests -------------");
+		System.out.println("");
+
+		System.out.println("--------------------------------------------");
+		System.out.println("#######------- FINISHED PROGRAM  -------####");
+		System.out.println("--------------------------------------------");
+
+	}
+
+	public static Map<String, Double[][]> learnFunction(double[][] X, double[][] Y) {
+
+		Map<String, Double[][]> weightMap = new HashMap<>();
+
+		int cycles = 4000;
+
+		int m = Y.length;
+		int nodes = X.length; // hidden layer size
+
+		double[][] W1 = Matrix.random(nodes, X[0].length);
+		double[][] b1 = new double[nodes][m];
+
+		double[][] W2 = Matrix.random(1, nodes);
+		double[][] b2 = new double[1][m];
+
+		X = Matrix.T(X);
+		Y = Matrix.T(Y);
+
+		for (int i = 0; i < cycles; i++) {
+
+			// Implement Forward Propagation to calculate A2 (probabilities)
+			// LAYER 1
+			double[][] Z1 = Matrix.add(Matrix.dot(W1, X), b1);
+			double[][] A1 = Matrix.sigmoid(Z1);
+			// double[][] A1 = Matrix.relu(Z1);
+			// LAYER 2
+			double[][] Z2 = Matrix.add(Matrix.dot(W2, A1), b2);
+			double[][] A2 = Matrix.sigmoid(Z2);
+			// double[][] A2 = Matrix.relu(Z2);
+
+			if (i == cycles - 1) {//store learned 
+				weightMap.put("W1", castPrimitiveDoubleToDouble(W1));
+				weightMap.put("W2", castPrimitiveDoubleToDouble(W2));
+				weightMap.put("B1", castPrimitiveDoubleToDouble(b1));
+				weightMap.put("B2", castPrimitiveDoubleToDouble(b2));
+			}
+
+			double cost = Matrix.cross_entropy(m, Y, A2);
+
+			// Back Prop
+			// LAYER 2
+			double[][] dZ2 = Matrix.subtract(A2, Y);
+			double[][] dW2 = Matrix.divide(Matrix.dot(dZ2, Matrix.T(A1)), m);
+			double[][] db2 = Matrix.divide(dZ2, m);
+			// LAYER 1
+			double[][] dZ1 = Matrix.multiply(Matrix.dot(Matrix.T(W2), dZ2), Matrix.subtract(1.0, Matrix.power(A1, 2)));
+			double[][] dW1 = Matrix.divide(Matrix.dot(dZ1, Matrix.T(X)), m);
+			double[][] db1 = Matrix.divide(dZ1, m);
+
+			// Gradient descent parameter update.
+			// Learning Rate lr
+			double lr = 0.01;
+			W1 = Matrix.subtract(W1, Matrix.multiply(lr, dW1));
+			b1 = Matrix.subtract(b1, Matrix.multiply(lr, db1));
+
+			W2 = Matrix.subtract(W2, Matrix.multiply(lr, dW2));
+			b2 = Matrix.subtract(b2, Matrix.multiply(lr, db2));
+
+			int c = cycles / 10;
+			if (i % c == 0) {
+				Matrix.print("============== Print after " + i + " Cycles.");
+				Matrix.print("Cost = " + cost);
+				Matrix.print("A2 = " + Arrays.deepToString(A2));
+			}
+
+		}
+
+		return weightMap;
+	}
+
+	public static void testLearnFunction(double[][] X, double[][] W1, double[][] W2, double[][] b1, double[][] b2) {
+		X = Matrix.T(X);
+		double[][] Z1 = Matrix.add(Matrix.dot(W1, X), b1);
+		double[][] A1 = Matrix.sigmoid(Z1);
+		double[][] Z2 = Matrix.add(Matrix.dot(W2, A1), b2);
+		double[][] A2 = Matrix.sigmoid(Z2);
+		Matrix.print("A2 = " + Arrays.deepToString(A2));
+	}
+
+	public static void testLearnFunction(double[] X, double[][] W1, double[][] W2, double[][] b1, double[][] b2) {
+		// Criamos aqui uma matrix que para passar por todos os treinos realisados
+		double[][] o = new double[W1.length][X.length];
+		// Preenche a matrix com o mesmo objeto para passar por cada treino
+		for (int i = 0; i < W1.length; i++) {
+			o[i] = X;
+		}
+		o = Matrix.T(o);
+		double[][] Z1 = Matrix.add(Matrix.dot(W1, o), b1);
+		double[][] A1 = Matrix.sigmoid(Z1);
+		double[][] Z2 = Matrix.add(Matrix.dot(W2, A1), b2);
+		double[][] A2 = Matrix.sigmoid(Z2);
+		Matrix.print("A2 = " + Arrays.deepToString(A2));
+	}
+
+	
+
+	public static Double[][] castPrimitiveDoubleToDouble(double[][] matrix) {
+		Double[][] matrixDouble = new Double[matrix.length][matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				matrixDouble[i][j] = matrix[i][j];
+			}
+		}
+		return matrixDouble;
+	}
+
+	public static double[][] castDoubleToPrimitiveDouble(Double[][] matrix) {
+		double[][] matrixDouble = new double[matrix.length][matrix[0].length];
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				matrixDouble[i][j] = matrix[i][j];
+			}
+		}
+		return matrixDouble;
+	}
+
+}
