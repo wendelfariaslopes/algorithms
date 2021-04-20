ebda06cae3ea13c6be159f7877898e3ce5a89b54
Wendel <wendellopesfarias@gmail.com>
2021-04-20 10:30:14 -0400

    ooo

--- src/core/java8/Lists.java
+++ src/core/java8/Lists.java
@@ -13,16 +13,33 @@ public class Lists {
 		
 		List<String> list = new ArrayList<>();
 		list.add("item 1");
-		list.add("item 2");
+		list.add("1");
+		list.add("item 1");
+		list.add("item 1");
+		list.add("item 1");
+		list.add("item 1");
+		list.add("item 1");
 		
-		String[] strings = convert(list);
-		for (String string : strings) {
-			System.out.println(string);
-		}
+//		String[] strings = convert(list);
+//		for (String string : strings) {
+//			System.out.println(string);
+//		}
+		
+		System.out.println(findInList(list));
+		
 	}
 	
 	public static String[] convert(List<String> strings) {
 		return strings.stream().toArray(String[]::new);
+	}
+	
+	
+	public static boolean findInList(List<String> list) {
+		for (String string : list) {
+			if(string.startsWith("PreTrade-BestFit"))
+				return true;
+		}
+		return false;
 	}
 
 }
